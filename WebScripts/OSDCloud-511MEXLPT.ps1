b7b94849c981caeb67cdb1c1beccbebe1cadfd69
Michael Escamilla <EskimoRuler@gmail.com>
2023-03-28 21:05:50 -0700

    Update OSDCloud-511MEXLPT.ps1

--- WebScripts/OSDCloud-511MEXLPT.ps1
+++ WebScripts/OSDCloud-511MEXLPT.ps1
@@ -17,7 +17,7 @@ Import-Module OSD -Force
 
 #Start OSDCloud ZTI the RIGHT way
 Write-Host -ForegroundColor Green "Start OSDCloud"
-
+Start-OSDCloud -ZTI -OSLanguage en-us -OSName 'Windows 11 22H2 x64' -OSEdition Enterprise -OSLicense Volume -Verbose
 
 #Restart from WinPE
 Write-Host -ForegroundColor Green "Restarting in 5 seconds!"
