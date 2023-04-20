2e3c009223991015cdbff5df92fbb4a2ddc91ed6
Michael Escamilla <EskimoRuler@gmail.com>
2023-04-20 13:30:17 -0700

    Update OSDCloud-TEST.ps1

--- WebScripts/OSDCloud-TEST.ps1
+++ WebScripts/OSDCloud-TEST.ps1
@@ -87,6 +87,7 @@ Write-Host -ForegroundColor Green "Create C:\Windows\System32\OOBE.cmd"
 $OOBECMD = @'
 PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
 Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
+Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
 Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
 Start /Wait PowerShell -NoL -C Start-OOBEDeploy
 Start /Wait PowerShell -NoL -C Restart-Computer -Force
