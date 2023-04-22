a082e2eee59b0b501ce68171694f558b2e9f6fff
Michael Escamilla <EskimoRuler@gmail.com>
2023-04-21 23:00:44 -0700

    Update oobetaskssetupV3.ps1

--- OOBE/oobetaskssetupV3.ps1
+++ OOBE/oobetaskssetupV3.ps1
@@ -17,6 +17,12 @@ $OOBEScript = @"
 `$Global:Transcript = "`$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-OOBEScripts.log"
 Start-Transcript -Path (Join-Path "`$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
 
+Write-Host -ForegroundColor DarkGray "Installing AutopilotOOBE PS Module"
+Start-Process PowerShell -ArgumentList "-NoL -C Install-Module AutopilotOOBE -Force -Verbose" -Wait
+
+Write-Host -ForegroundColor DarkGray "Installing OSD PS Module"
+Start-Process PowerShell -ArgumentList "-NoL -C Install-Module OSD -Force -Verbose" -Wait
+
 Write-Host -ForegroundColor DarkGray "Executing OOBEDeploy Script fomr OSDCloud Module"
 Start-Process PowerShell -ArgumentList "-NoL -C Start-OOBEDeploy" -Wait
 
