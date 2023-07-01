c89d0258e055a59a3c68321658cfb0758ee895a7
Michael Escamilla <EskimoRuler@gmail.com>
2023-07-01 12:29:36 -0700

    Update Add-Wim.ps1

--- Setup/Add-Wim.ps1
+++ Setup/Add-Wim.ps1
@@ -9,9 +9,12 @@ New-OSDCloudWorkspace -WorkspacePath "$WorkspacePath"
 # Set the directories to keep
 $KeepTheseDirs = @('boot','efi','en-us','sources','fonts','resources')
 # Clean the below directories
-Get-ChildItem "$($WorkspacePath)\Media" | Where {$_.PSIsContainer} | Where {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force
-Get-ChildItem "$($WorkspacePath)\Media\Boot" | Where {$_.PSIsContainer} | Where {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force
-Get-ChildItem "$($WorkspacePath)\Media\EFI\Microsoft\Boot" | Where {$_.PSIsContainer} | Where {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force
+Write-Host "Deleting files from: $($WorkspacePath)\Media"
+Get-ChildItem "$($WorkspacePath)\Media" | Where-Object {$_.PSIsContainer} | Where-Object {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force
+Write-Host "Deleting files from: $($WorkspacePath)\Media\Boot"
+Get-ChildItem "$($WorkspacePath)\Media\Boot" | Where-Object {$_.PSIsContainer} | Where-Object {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force
+Write-Host "Deleting files from: $($WorkspacePath)\Media\EFI\Microsoft\Boot"
+Get-ChildItem "$($WorkspacePath)\Media\EFI\Microsoft\Boot" | Where-Object {$_.PSIsContainer} | Where-Object {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force
 
 # Edit WinPE and rebuild ISO
-Edit-OSDCloudWinPE -Wallpaper "C:\_OSDCloud\Wallpaper\511-WindowsPE-DesktopBackground-V2.jpg" -StartURL 'https://raw.githubusercontent.com/EskimoRuler/Powershell-OSDCloud/main/OSDCloud-511.ps1'
+#Edit-OSDCloudWinPE -Wallpaper "C:\_OSDCloud\Wallpaper\511-WindowsPE-DesktopBackground-V2.jpg" -StartURL 'https://raw.githubusercontent.com/EskimoRuler/Powershell-OSDCloud/main/OSDCloud-511.ps1'
